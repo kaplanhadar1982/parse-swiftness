dc7d58940e8a924300390f2dd1b6f44ac1394716
kaplan hadar <kaplanhadar@gmail.com>
2018-12-20 16:07:43 +0200

    start mapping 2

--- testexcel.py
+++ testexcel.py
@@ -8,10 +8,10 @@ worksheet = workbook.add_worksheet()
 
 # Some data we want to write to the worksheet.
 expenses = (
-    ['Rent', 1000],
-    ['Gas',   100],
-    ['Food',  300],
-    ['Gym',    50],
+    ['Rent', 1000,1],
+    ['Gas',   100,4],
+    ['Food',  300,5],
+    ['Gym',    50,6],
 )
 
 # Start from the first cell. Rows and columns are zero indexed.
@@ -19,9 +19,10 @@ row = 0
 col = 0
 
 # Iterate over the data and write it out row by row.
-for item, cost in (expenses):
+for item, cost,t in (expenses):
     worksheet.write(row, col,     item)
     worksheet.write(row, col + 1, cost)
+    worksheet.write(row, col + 2, t)
     row += 1
 
 # Write a total using a formula.
